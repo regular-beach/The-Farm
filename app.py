1291d2e0157d6e0f09e0e94c3db8da19dcf375bc
regular beach <renconger@gmail.com>
2025-01-10 18:48:10 -0800

    Commit message

--- app.py
+++ app.py
@@ -11,77 +11,10 @@ app.debug = False
 def index():
     return render_template('index.html')
 
-@app.route('/projects')
-def projects():
-    return render_template('projects.html', projects=projects)
 
-
-
-@app.route('/project/<project_name>')
-def project(project_name):
-    # Find the project by name in your `projects` list
-    project = next((p for p in projects if p["name"] == project_name), None)
-    if project is None:
-        return "Project not found", 404
-    
-    # Render the specific template for this project within the `projects` subdirectory
-    try:
-        return render_template(f'projects/{project_name}.html', project=project)
-    except Exception as e:
-        # Handle missing or misnamed templates
-        return f"Error loading project template: {e}", 404
-
-
-
-@app.route('/video')
-def video():
-    return render_template('video.html')
-
-@app.route('/buy')
-def buy():
-    return render_template('buy.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/shop1')
-def shop1():
-    return render_template('shop1.html')
-
-
-projects = [
-    {
-        "name": "letterprints",
-        "date": "'23 - present",
-        "medium": "lino-cut",
-        "artist": "reg-beach",
-        "description": "Regular Beach experimental type project no.1."
-    },
-    {
-        "name": "photofeature1",
-        "date": "november '24",
-        "medium": "35mm",
-        "artist": "reg-beach",
-        "description": "12 imgs from 2 rolls of 35mm."
-    },
-    #{
-        #"name": "photofeature2",
-       # "date": "november '24",
-       # "medium": "iphone",
-        #"artist": "july guzman",
-       # "description": "informing artistic practice."
-   # }
-    
-    # Add more projects as needed
-]
-
-
-#if __name__ == '__main__':
- #app.run(host='0.0.0.0') 
   
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=8000)
+    serve(app, host="0.0.0.0", port=4000)
 
 
     
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/templates/index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
