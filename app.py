fd194fa61c8ac0790ee47e19a4edd50a89556543
regular beach <renconger@gmail.com>
2025-01-15 13:14:03 -0800

    testing

--- app.py
+++ app.py
@@ -53,8 +53,9 @@ def home():
 
 
   
-if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=4000)
+#if __name__ == "__main__":
+   # serve(app, host="0.0.0.0", port=4000)
 
 
-    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0') 
