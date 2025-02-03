d667ce00a71cebbdd872c95ca3fe9d528febdff7
regular beach <renconger@gmail.com>
2025-02-03 09:38:21 -0800

    app.py prep

--- app.py
+++ app.py
@@ -55,9 +55,9 @@ def flickity():
 
 
   
-if __name__ == "__main__":
-   serve(app, host="0.0.0.0", port=4000)
+#if __name__ == "__main__":
+   #serve(app, host="0.0.0.0", port=4000)
 
 
-#if __name__ == '__main__':
-   # app.run(host='0.0.0.0') 
+if __name__ == '__main__':
+   app.run(host='0.0.0.0') 
